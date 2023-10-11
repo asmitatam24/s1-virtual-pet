c4abe0dd786128b38e6a0215960c83f30b69c668
asmitat24 <asmitat24@ahs.local>
2023-10-11 14:08:38 -0700

    ss

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,8 +1,33 @@
-public class Runner{
-    public static void main(String[] args){
-        VirtualPet p = new VirtualPet();
+import javax.swing.*;
 
-        p.feed();
+public class Runner{
+    public Runner(){
+        VirtualPet p = new VirtualPet();
         p.exercise();
+        stop(4000);
+        p.feed();
+        String ans = response("How was your day");
+        System.out.println(ans);
+    }
+
+    public String response(String q){
+        String s = (String)JOptionPane.showInputDialog(
+            new JFrame(),
+            q,
+            "Get a response",
+            JOptionPane.PLAIN_MESSAGE
+        );
+        return s;
+    }
+
+    public void stop(int milliseconds){
+        try{
+            Thread.sleep(milliseconds);
+        } catch(Exception e){
+
+        }
+    }
+    public static void main(String[] args){
+        new Runner();
     }
 }
